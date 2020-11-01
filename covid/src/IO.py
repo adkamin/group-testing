175ac81161572e635b0464dc690a3a818716ae37
adkamin <minichovaa@gmail.com>
2020-11-01 14:47:38 +0100

    Server communication

--- covid/src/IO.py
+++ covid/src/IO.py
@@ -3,6 +3,12 @@ from bronkerbosh import find_maximal_cliques
 
 
 def IO(input_file):
+    # read_graph_input(input_file)
+    read_graph_server()
+
+
+# For testing purposes
+def read_graph_input(input_file):
     number_of_tests = int(input_file.readline())
     while number_of_tests > 0:
         stats = Statistics()
@@ -19,8 +25,28 @@ def IO(input_file):
             number_of_edges -= 1
         stats.graph.create_nodes(nodes)
         number_of_tests -= 1
-        #print(stats)
-        #print('finding cliques...')
-        find_maximal_cliques(stats.graph)
-        # Choose cliques with most unique nodes first and test
         break
+
+# To communicate with the server
+def read_graph_server():
+    number_of_tests = int(input())
+    while number_of_tests > 0:
+        stats = Statistics()
+        nodes = list(range(int(input())))
+        number_of_edges = int(input())
+        stats.initially_infected = int(input())
+        stats.infection_chance = float(input())
+        bounds = input().split(' ')
+        stats.lower_bound = int(bounds[0])
+        stats.upper_bound = int(bounds[1])
+        while number_of_edges > 0:
+            edge = input().split(' ')
+            stats.graph.edges.append((int(edge[0]), int(edge[1])))
+            number_of_edges -= 1
+        stats.graph.create_nodes(nodes)
+        number_of_tests -= 1
+        print("test 0")
+        answer = input()
+        print("answer 0")
+        answer = input()
+        # TODO find out how to send tests in bulk
--- covid/src/main.py
+++ covid/src/main.py
@@ -2,38 +2,23 @@ from graph import Graph
 from IO import IO
 
 
-
-# def main():
-#     # Read file and save into list of doubles
-#     with open("src/input", 'r') as inputfile:
-#         lst = inputfile.read()
-#         lst = [float(i) for i in lst.split()]
-
-#     # Create graph from the list
-#     g = Graph()
-#     g.nodes = list(range(int(lst[0])))
-#     index = 5
-#     for i in range(int(lst[1])):
-#         edge = (lst[index],lst[index+1])
-#         g.edges.append(edge)
-#         index += 2
-#     print(g.edges)
-#     print(g.nodes)
-
 def main():
-    while True:
-        try:
-            # path = input("Please enter a input path:\n")
-            path = "src/input"
-            with open(path, 'r') as input_file:
-                IO(input_file)
-                break
-        except (FileNotFoundError, IOError):
-            print("Error: file not found")
-        else:
-            break
-        
-    
+    # Code for testing with inputfile
+    # while True:
+    #     try:
+    #         # path = input("Please enter a input path:\n")
+    #         path = "src/input"
+    #         with open(path, 'r') as input_file:
+    #             IO(input_file)
+    #             break
+    #     except (FileNotFoundError, IOError):
+    #         print("Error: file not found")
+    #     else:
+    #         break
+
+    # Code for testing with server
+    inputfile = "src/input" # dummy
+    IO(inputfile)
 
 
 if __name__ == '__main__':
