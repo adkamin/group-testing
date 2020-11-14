5f30f17c81ebd15c1a9a2c89389a6181834e74e1
Thomas <thomaskolb98@gmail.com>
2020-11-14 13:09:48 +0100

    smaller alg

--- src/algorithm.py
+++ src/algorithm.py
@@ -105,30 +105,22 @@ def binary_search(nodes, left_half):
 def connected_tuples(pairs):
     # for every element, we keep a reference to the list it belongs to
     lists_by_element = {}
-
-    def make_new_list_for(x, y):
-        lists_by_element[x] = lists_by_element[y] = [x, y]
-
-    def add_element_to_list(lst, el):
-        lst.append(el)
-        lists_by_element[el] = lst
-
-    def merge_lists(lst1, lst2):
-        merged_list = lst1 + lst2
-        for el in merged_list:
-            lists_by_element[el] = merged_list
-
+    
     for x, y in pairs:
         xList = lists_by_element.get(x)
         yList = lists_by_element.get(y)
         if not xList and not yList:
-            make_new_list_for(x, y)
+            lists_by_element[x] = lists_by_element[y] = [x, y]
         if xList and not yList:
-            add_element_to_list(xList, y)
+            xList.append(y)
+            lists_by_element[y] = xList
         if yList and not xList:
-            add_element_to_list(yList, x)
+            yList.append(x)
+            lists_by_element[x] = yList
         if xList and yList and xList != yList:
-            merge_lists(xList, yList)
+            merged_list = xList + yList
+            for el in merged_list:
+                lists_by_element[el] = merged_list
     # return the unique lists present in the dictionary
     # print("Groups: " + str(set(tuple(l) for l in lists_by_element.values())), file=sys.stderr)
     return set(tuple(l) for l in lists_by_element.values())
